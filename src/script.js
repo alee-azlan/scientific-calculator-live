a679a99d7646489ca180ab97bfc2fa6cb3c96f91
alee-azlan <aliazlan20044@gmail.com>
2024-07-08 10:08:12 +0000

    scientific functions

--- src/script.js
+++ src/script.js
@@ -9,7 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
             const convertedvalue= currentvalue
             .replace("×","*")
             .replace("÷","/")
-            .replace("%","*0.01");
+            .replace("%","*0.01")
+            .replace("sin","Math.sin")
+            .replace("cos" , "Math.cos")
+            .replace("ln","Math.log")
+            .replace("π","Math.PI")
+            .replace("log","Math.log10")
+            .replace("e","Math.E")
+            .replace("tan","Math.tan")
+            .replace("√","Math.");
             console.log("ConvertedValue:" , convertedvalue);
             
         const result = eval(convertedvalue);
@@ -21,20 +29,28 @@ document.addEventListener("DOMContentLoaded", function () {
         const button = buttons[i];
         button.addEventListener('click', function () {
             const value = button.innerText;
-
-            if (value == "AC") {
-                currentvalue = "";
-                display.value = currentvalue;
-            } 
-            else if(value == "=")
-            {
-                evaluateResult();
-            } 
-            else {
-                currentvalue += value;
-                console.log("Current Value:", currentvalue);
-                display.value = currentvalue;
+            try{
+                if (value == "AC") {
+                    currentvalue = "";
+                    display.value = currentvalue;
+                } 
+                else if(value == "=")
+                {
+                    evaluateResult();
+                } 
+                else {
+                    currentvalue += value;
+                    console.log("Current Value:", currentvalue);
+                    display.value = currentvalue;
+                }
             }
+                catch (error){
+                    console.error(error);
+                    currentvalue="ERROR";
+                    display.value= currentvalue;
+                }
+            
+           
         })
     }
 });
